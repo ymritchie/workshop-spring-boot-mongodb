8383babef47805c35f81c34bc9ad08a8df08af80
Yanisley Mora Ritchie <bbpf0415@BBPGETEC415.bbp.com.br>
2019-06-13 09:21:07 -0300

    Tratamento de erro personalizado

--- src/main/java/com/ritchie/worshopmongo/services/UserService.java
+++ src/main/java/com/ritchie/worshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ritchie.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ritchie.worshopmongo.domain.User;
 import com.ritchie.worshopmongo.repository.UserRepository;
+import com.ritchie.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
