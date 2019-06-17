873cb963642b9d58c1c1bae707f1d436c726a5d0
Yanisley Mora Ritchie <bbpf0415@BBPGETEC415.bbp.com.br>
2019-06-17 16:19:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ritchie/worshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.ritchie.worshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ritchie.worshopmongo.domain.Post;
+import com.ritchie.worshopmongo.repository.PostRepository;
+import com.ritchie.worshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
